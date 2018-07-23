2f75142b604ae1b59024ab6adb04d5493ca5d5fd
imtiaz <imtiazmiskin@gmail.com>
2018-07-23 21:43:32 +1000

    new changes

---
+++ TrnArea.cs
@@ -0,0 +1,7 @@
+public class TrnArea
+{
+	public static void main(string[] args)
+	{
+		Console.WriteLine("Hello World");
+	}		
+}
--- TrnCountry.cs
+++ TrnCountry.cs
@@ -1,7 +1,13 @@
 public class TrnCountry
 {
+	string CountryName;
 	public static void main(string[] args)
 	{
 		Console.WriteLine("Hello World");
-	}		
+	}	
+	
+	public GetCountry(string CountryId)
+	{
+		return this.CountryName;
+	}	
 }
